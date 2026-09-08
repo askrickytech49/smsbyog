6442cb14f56c54aabcea8e2a04043b6cde9bbed0
Ricky <askrickytech49@gmail.com>
2026-09-08 17:55:32 +0100

    Fix admin panel asset paths for Hostinger deployment

--- admin/include/head.php
+++ admin/include/head.php
@@ -1,12 +1,15 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="getallscripts Developer">
+  <meta name="description" content="SmsByOg Admin">
   <meta name="author" content="">
-  <link href="https://myogsms.com/favicon.png" rel="icon">
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <link href="css/ruang-admin.min.css" rel="stylesheet">
-  <link href="libs/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css" /> 
+  <link href="<?= $site_data['logo_url'] ?? '' ?>" rel="icon">
+  <?php
+    // Build correct base path — works on both localhost/smsbyog and live domain root
+    $admin_base = rtrim(dirname(dirname($_SERVER['PHP_SELF'])), '/') . '/admin/';
+  ?>
+  <link href="<?= $admin_base ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?= $admin_base ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="<?= $admin_base ?>css/ruang-admin.min.css" rel="stylesheet">
+  <link href="<?= $admin_base ?>libs/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css">
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-             
--- admin/include/script.php
+++ admin/include/script.php
@@ -1,7 +1,9 @@
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="js/ruang-admin.min.js"></script>
-  <script src="js/notiflix-aio-2.7.0.min.js"></script>
-  <script src="libs/sweetalert2/sweetalert2.min.js"></script> 
-                      
+<?php
+  $admin_base = rtrim(dirname(dirname($_SERVER['PHP_SELF'])), '/') . '/admin/';
+?>
+  <script src="<?= $admin_base ?>vendor/jquery/jquery.min.js"></script>
+  <script src="<?= $admin_base ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= $admin_base ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?= $admin_base ?>js/ruang-admin.min.js"></script>
+  <script src="<?= $admin_base ?>js/notiflix-aio-2.7.0.min.js"></script>
+  <script src="<?= $admin_base ?>libs/sweetalert2/sweetalert2.min.js"></script>
