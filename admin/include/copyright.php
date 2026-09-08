ba005a7ba7cda89a13e361915b81274056e4c11a
Ricky <askrickytech49@gmail.com>
2026-09-08 23:41:00 +0100

    Update footer credit to Askricky.Tech with WhatsApp link

--- admin/include/copyright.php
+++ admin/include/copyright.php
@@ -2,7 +2,7 @@
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
             <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
-              <b><a href="https://wa.me/2347015367977" target="_blank">Sammyloaded.dev</a></b>
+              <b><a href="https://wa.me/2349129403787" target="_blank">Askricky.Tech</a></b>
             </span>
           </div>
         </div>
