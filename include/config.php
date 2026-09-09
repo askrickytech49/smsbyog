e2b4380b574b52a982b4f472db28efede09ff58a
Ricky <askrickytech49@gmail.com>
2026-09-09 20:04:04 +0100

    Update DB credentials for user.smsbyog.com cPanel deployment

--- include/config.php
+++ include/config.php
@@ -5,10 +5,10 @@ header("Expires: 0"); // Proxies.
 //  error_reporting(0);
 date_default_timezone_set('Africa/Lagos');
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'u223553481_smsbyog_user');
-define('DB_PASSWORD', '~u9AycO=C');
-define('DB_DATABASE', 'u223553481_smsbyog');
+define('DB_SERVER', 'localhost'); //localhost
+define('DB_USERNAME', 'smsbyogc_ricky'); // db username
+define('DB_PASSWORD', 'RM*l6OcqK[eLMfa7'); // db password
+define('DB_DATABASE', 'smsbyogc_app'); // db name
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 if ($conn->connect_error) {
